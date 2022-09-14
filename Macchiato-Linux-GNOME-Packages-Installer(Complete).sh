08eaff45ac65e9c4d053baef603fdbc09f57d548
Macchiato-Linux <111928557+Macchiato-Linux@users.noreply.github.com>
2022-09-14 12:17:44 -0500

    Add files via upload

---
+++ Macchiato-Linux-GNOME-Packages-Installer(Complete).sh
@@ -0,0 +1,29 @@
+# This file is a file from the Macchiato-Linux-GNOME-Packages 
+# repository for Macchiato Linux Standard.
+
+#!/bin/bash
+sudo apt install nautilus
+sudo apt install epiphany-browser
+sudo apt install totem
+sudo apt install geary
+sudo apt install gnome-terminal
+sudo apt install gnome-calendar
+sudo apt install gnome-software gnome-packagekit
+sudo apt install gnome-characters
+sudo apt install gnome-clocks
+sudo apt install gnome-contacts
+sudo apt install gparted
+sudo apt install evince
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install rhythmbox
+sudo apt install gnome-maps
+sudo apt install gedit
+sudo apt install gnome-system-monitor
+sudo apt install gnome-weather
+sudo apt install gnome-boxes
+sudo apt install deluge
+sudo apt install deja-dup
+sudo apt install geany
+sudo apt install gnome-chess
+sudo apt insall gnome-dictionary
---
+++ Macchiato-Linux-GNOME-Packages-Installer(Core).sh
@@ -0,0 +1,9 @@
+# This file is a file from the Macchiato-Linux-GNOME-Packages 
+# repository for Macchiato Linux Core.
+
+#!/bin/bash
+sudo apt install nautilus
+sudo apt install epiphany-browser
+sudo apt install totem
+sudo apt install geary
+sudo apt install gnome-terminal
